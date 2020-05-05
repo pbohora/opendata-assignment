6247aafdf15a0e5fff8f9bd7b883c90e8d5f0b42
pbohora <pradeepbohora26@gmail.com>
2020-05-05 15:47:28 +0300

    create mongoose model for data

--- routes/data.js
+++ routes/data.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
 const dataRouter = require('express').Router();
+const request = require('request');
+const token = process.env.TOKEN;
 
 dataRouter.get('/', (req, res) => {
   res.send('Hello data');
