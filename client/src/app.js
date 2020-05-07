569b9c659f47ef4f0873f4db3a21259373e63529
pbohora <pradeepbohora26@gmail.com>
2020-05-07 16:39:55 +0300

    fix error in usereffect

--- client/src/app.js
+++ client/src/app.js
@@ -13,7 +13,7 @@ import {
 
 const App = () => {
   const [openData, setOpenData] = useState([]);
-  //   const [timestamp, setTimestamp] = useState([]);
+  const [timestamp, setTimestamp] = useState([]);
 
   useEffect(() => {
     getOpenData().then((data) => {
@@ -22,24 +22,6 @@ const App = () => {
     });
   }, []);
 
-  const timestamp = openData.map((data) => {
-    return new Date(data.createdAt).getHours();
-  });
-  console.log(timestamp);
-  const ONE_DAY = 86400000;
-
-  const data = [
-    { x: 20, y: 8 },
-    { x: 1, y: 5 },
-    { x: 2, y: 4 },
-    { x: 3, y: 9 },
-    { x: 4, y: 1 },
-    { x: 5, y: 7 },
-    { x: 6, y: 6 },
-    { x: 7, y: 3 },
-    { x: 8, y: 2 },
-    { x: 9, y: 0 },
-  ];
   return (
     <div className='App'>
       {timestamp && (
